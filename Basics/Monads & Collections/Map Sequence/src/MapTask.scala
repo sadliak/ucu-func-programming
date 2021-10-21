15dbd97cb106c2569e9dacd8974469d88068a619
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-21 23:12:45 +0300

    Add MapTask task solution

--- Basics/Monads & Collections/Map Sequence/src/MapTask.scala
+++ Basics/Monads & Collections/Map Sequence/src/MapTask.scala
@@ -1,4 +1,9 @@
+import scala.util.Try
+
 object MapTask {
   def mapping(input: Seq[String]): Seq[Int] =
-    ???
+    input
+      .map(el => Try(el.toInt))
+      .filter(el => el.isSuccess)
+      .map(el => el.get)
 }
