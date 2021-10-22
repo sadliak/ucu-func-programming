4fffe3fbe12e4497b5793bc270202b532c38157f
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 20:35:19 +0300

    Add Vector Math task solution

--- Basics/Typing & Generics/Vector Math/src/VectorTask.scala
+++ Basics/Typing & Generics/Vector Math/src/VectorTask.scala
@@ -1,33 +1,42 @@
 object VectorTask {
 
-	trait Vector/* GENERIC */ {
+  trait Vector[In] {
 
-		/**
-		 * Get value of the vector at specific position
-		 *
-		 * @param position of vector value. Must be less than `size`
-		 * @return value at the given position */
-		def apply(position: Int): Float
+    /**
+     * Get value of the vector at specific position
+     *
+     * @param position of vector value. Must be less than `size`
+     * @return value at the given position */
+    def apply(position: Int): Float
 
-		/**
-		 * Create a new Vector instance with the given value and previous values
-		 *
-		 * @param position of vector value. Must be less than `size`
-		 * @param value    the value to set
-		 * @return new instance of this vector */
-		def modify(position: Int, value: Float): Vector
+    /**
+     * Create a new Vector instance with the given value and previous values
+     *
+     * @param position of vector value. Must be less than `size`
+     * @param value    the value to set
+     * @return new instance of this vector */
+    def modify(position: Int, value: Float): Vector[In]
 
-		/**
-		 * Provides the size of this vector
-		 *
-		 * @return actual size of the vector */
-		def size(): Int
-	}
+    /**
+     * Provides the size of this vector
+     *
+     * @return actual size of the vector */
+    def size(): Int
+  }
 
-	def sum/* GENERIC */(vector1: Vector, vector2: Vector): Vector = ???
+  def sum[In](vector1: Vector[In], vector2: Vector[In]): Vector[In] = {
+    if (vector1.size() != vector2.size()) {
+      throw new RuntimeException("Vector size mismatch")
+    }
 
-	def sub/* GENERIC */(vector1: Vector, vector2: Vector): Vector = ???
+    Range(0, vector1.size(), 1).foldLeft(vector1)((v, idx) => v.modify(idx, v(idx) + vector2(idx)))
+  }
 
-	
+  def sub[In](vector1: Vector[In], vector2: Vector[In]): Vector[In] = {
+    if (vector1.size() != vector2.size()) {
+      throw new RuntimeException("Vector size mismatch")
+    }
 
+    Range(0, vector1.size(), 1).foldLeft(vector1)((v, idx) => v.modify(idx, v(idx) - vector2(idx)))
+  }
 }
