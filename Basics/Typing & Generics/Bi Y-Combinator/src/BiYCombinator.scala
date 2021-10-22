7909cbc549fe35ec4e596abc07827d94ca129c60
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 20:48:43 +0300

    Add Bi Y-Combinator task solution

--- Basics/Typing & Generics/Bi Y-Combinator/src/BiYCombinator.scala
+++ Basics/Typing & Generics/Bi Y-Combinator/src/BiYCombinator.scala
@@ -1,5 +1,7 @@
 object BiYCombinator {
 
-	def fix/* GENERICS */(bifnFactory: Any): Any =
-		???
+	def Y[A, B](f: (A => B) => A => B): A => B = f(Y(f))
+
+	def fix[In1, In2, Out](biFnFactory: ((In1, In2) => Out) => (In1, In2) => Out): (In1, In2) => Out =
+		(x: In1, y: In2) => biFnFactory(fix(biFnFactory))(x, y)
 }
