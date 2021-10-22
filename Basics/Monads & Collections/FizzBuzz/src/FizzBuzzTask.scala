71117fe048f2c93775985f54f0dd87e31eb2f31e
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 18:21:43 +0300

    Add FizzBuzz task solution

--- Basics/Monads & Collections/FizzBuzz/src/FizzBuzzTask.scala
+++ Basics/Monads & Collections/FizzBuzz/src/FizzBuzzTask.scala
@@ -1,4 +1,9 @@
 object FizzBuzzTask {
   def fizzBuzzIt(s: Seq[Int]): Seq[String] =
-    ???
+    s.map {
+      case x if x % 15 == 0 => "FizzBuzz"
+      case x if x % 3 == 0 => "Fizz"
+      case x if x % 5 == 0 => "Buzz"
+      case x => x.toString
+    }
 }
