b2b9fe78c4cf4ba1cb6b44d23645ab29bdafc0a0
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 18:16:16 +0300

    Add Take Words task solution

--- Basics/Monads & Collections/Take Words/src/TakeWordsTask.scala
+++ Basics/Monads & Collections/Take Words/src/TakeWordsTask.scala
@@ -1,7 +1,23 @@
-import scala.annotation.tailrec
-
 object TakeWordsTask {
   val lineSeparatorString = "\n"
 
-  def takeWords(seq: Seq[String]): Seq[String] = ???
+  def takeWords(seq: Seq[String]): Seq[String] = {
+    @annotation.tailrec
+    def takeWordsTailRec(seq: Seq[String], newSeq: Seq[String], separator: String, seenSeparator: Boolean): Seq[String] =
+      (seq, seenSeparator) match {
+        case (Nil, _) => newSeq
+        case (_, true) => newSeq
+        case (x :: xs, false) =>
+          val word = x.takeWhile(ch => ch.toString != separator)
+
+          takeWordsTailRec(
+            xs,
+            if (word.nonEmpty) newSeq :+ word else newSeq,
+            separator,
+            x != word
+          )
+      }
+
+    takeWordsTailRec(seq, Seq(), lineSeparatorString, seenSeparator = false)
+  }
 }
