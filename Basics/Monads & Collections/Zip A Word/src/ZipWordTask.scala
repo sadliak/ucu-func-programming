16b231d70f8689fc798066c31f1943d8a45cefcd
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 18:38:15 +0300

    Add Zip A Word task solution

--- Basics/Monads & Collections/Zip A Word/src/ZipWordTask.scala
+++ Basics/Monads & Collections/Zip A Word/src/ZipWordTask.scala
@@ -1,3 +1,4 @@
 object ZipWordTask {
-  def zipIt(prefixSeq: Seq[String], wordSeq: Seq[String], suffixSeq: Seq[String]): Seq[String] = ???
+  def zipIt(prefixSeq: Seq[String], wordSeq: Seq[String], suffixSeq: Seq[String]): Seq[String] =
+    (prefixSeq, wordSeq, suffixSeq).zipped.map((prefix, word, suffix) => s"$prefix$word$suffix")
 }
