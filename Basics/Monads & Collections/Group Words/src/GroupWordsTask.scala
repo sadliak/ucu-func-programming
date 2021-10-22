c72bdd5397394193285a79f8de7fb4e46cad3b35
Oleksii Sadliak <o.sadliak@gmail.com>
2021-10-22 18:33:19 +0300

    Add Group Words task solution

--- Basics/Monads & Collections/Group Words/src/GroupWordsTask.scala
+++ Basics/Monads & Collections/Group Words/src/GroupWordsTask.scala
@@ -1,3 +1,7 @@
 object GroupWordsTask {
-  def groupIt(wordsSeq: Seq[String]): Map[Char, Int] = ???
+  def groupIt(wordsSeq: Seq[String]): Map[Char, Int] =
+    wordsSeq
+      .map(word => (word.head, word))
+      .groupBy(_._1)
+      .mapValues(_.map(value => value._2.count(ch => ch == value._1)).sum)
 }
