c0e749440e3cdeb1c45e17af8652e33c0eb66d3b
Oleksii Sadliak <o.sadliak@gmail.com>
2021-11-25 03:46:55 +0200

    Add WikiIntegrationTask solution

--- Advanced/Parallel & Async/Wiki Integration/src/WikiIntegrationTask.scala
+++ Advanced/Parallel & Async/Wiki Integration/src/WikiIntegrationTask.scala
@@ -9,13 +9,34 @@ import akka.http.scaladsl.unmarshalling.Unmarshal
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.Future
 import scala.xml.NodeSeq
 
 object WikiIntegrationTask {
 
-  def retrieveLatestChanges(): Future[Seq[WikiChange]] = ???
+  def retrieveLatestChanges(): Future[Seq[WikiChange]] = {
+    implicit val actorSystem = ActorSystem(Behaviors.empty, "http")
+    implicit val executionContext = actorSystem.executionContext
+
+    val request = HttpRequest(
+      method = HttpMethods.GET,
+      uri = Uri("https://www.mediawiki.org/w/api.php").withQuery(Query(("action", "feedrecentchanges"))),
+      headers = Accept(MediaTypes.`application/xml`) :: Nil
+    )
+
+    Http()
+      .singleRequest(request)
+      .map(response => response.entity)
+      .flatMap(entity => Unmarshal(entity).to[NodeSeq])
+      .map(xml => (xml \ "channel" \ "item")
+        .map(node => WikiChange(
+          title = (node \ "title").text,
+          link = Uri((node \ "link").text),
+          guid = Uri((node \ "guid").text),
+          description = (node \ "description").text,
+          pubDate = LocalDateTime.parse((node \ "pubDate").text, DateTimeFormatter.RFC_1123_DATE_TIME),
+        )))
+  }
 
   case class WikiChange(title: String, link: Uri, guid: Uri, description: String, pubDate: LocalDateTime)
-
 }
